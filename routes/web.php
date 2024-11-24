e205c49f0645d792cbfeb7a688d46e78e3b206f8
or gabay <orgabai1212@gmail.com>
2024-11-24 10:53:07 -0500

    3

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
